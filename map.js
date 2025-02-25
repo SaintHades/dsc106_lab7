4eef19a3bff0701a50344f95ced8147e139b67f0
SaintHades <hardy.ling031022@gmail.com>
2025-02-25 02:27:25 -0800

    add map

---
+++ map.js
@@ -0,0 +1,14 @@
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'YOUR_ACCESS_TOKEN_HERE';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18 // Maximum allowed zoom
+});
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FpbnRoYWRlcyIsImEiOiJjbTdrYzNtZ2IwNWtqMmlweGh0cGpsdGxzIn0.iih054pEPqopwdLFBu1S3w';
